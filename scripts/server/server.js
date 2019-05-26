891618d019a948d505a5d7b9927029e1bd9f6358
konrad2002 <konrad@schwimmteamerzgebirge.de>
2019-05-26 17:54:20 +0200

    rocket auto fly when has tag

--- scripts/server/server.js
+++ scripts/server/server.js
@@ -5,3 +5,9 @@ system.initialize = function() {};
 system.update = function() {
     this.runCommand("function rocketFlight");
 }
+
+system.runCommand = function(command) {
+    commandData = this.createEventData("minecraft:execute_command")
+    commandData.data.command = command
+    this.broadcastEvent("minecraft:execute_command", commandData)
+}
